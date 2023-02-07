295db3f0fe4395f07df962d170f0ddb1107a6492
Mary Metcalfe <G00425666@atu.ie>
2023-02-07 03:30:44 +0000

    \Practice

---
+++ Variables2.py
@@ -0,0 +1,21 @@
+# gaining some insight
+# i will master this
+# Author : MAry Metcalfe
+
+kids = ("Angie", "Willow", "Eryk")
+
+x , y , z = kids
+
+print (x)
+print (y)
+print (z)
+
+a = " Angie "
+b = " is "
+c = " scary"
+print (a + b + c)
+
+Angie = 12
+Willow = 10
+
+print ( Angie + Willow)
