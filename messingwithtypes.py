58e02efca97cab3db45967df0126fb3ab7e260f2
Mary Metcalfe <G00425666@atu.ie>
2023-02-08 21:57:15 +0000

    \mywork

---
+++ messingwithtypes.py
@@ -0,0 +1,10 @@
+# messing with variables/types
+# author : Mary Metcalfe
+
+answer_of_the_question_is = 43
+print (f"the answer is {answer_of_the_question_is}")
+
+print (f"type of answer is {type(answer_of_the_question_is)}")
+
+
+
