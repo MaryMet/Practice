19c58ab0116d09169ad3174477c4a3fbfcb0256a
Mary Metcalfe <G00425666@atu.ie>
2023-02-09 17:43:26 +0000

    \

--- File Week 3/sub.py
+++ File Week 3/sub.py
@@ -5,3 +5,10 @@
 ans = (10 -4)
 print ("the answer is " + str(ans))
 print (f"the answer is {ans}")
+
+x = int (input ("please enter first number:"))
+y = int (input ("please enter second number:"))
+
+answer = (x-y)
+
+print (f"the answer is {answer}")
