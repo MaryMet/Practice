2323f88f37dabf31d4df6b3ada6163efb6755f1d
Mary Metcalfe <G00425666@atu.ie>
2023-02-11 12:54:01 +0000

    \

--- File Week 3/absolute.py
+++ File Week 3/absolute.py
@@ -3,6 +3,6 @@
 
 number = float(input("Enter a number : "))
 
-absolute_value = abs (number)
+absolute_value = abs(number)
 
-print ("The absolute value of {} is {} " .format(normal, absolute_value))
+print ("The absolute value of {} is {}".format(number, absolute_value))
---
+++ floor.py
@@ -0,0 +1,11 @@
+# flooring a number
+# Author : MAry Metcalfe
+import math
+
+
+
+number_to_floor = float(input("enter a number to floor: "))
+
+floored_number = math.floor(number_to_floor)
+
+print ('{} floored is {}' .format (number_to_floor,floored_number))
