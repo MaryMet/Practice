c0c03a0d99c35672758c7a37e232f0070b2ced18
Mary Metcalfe <G00425666@atu.ie>
2023-02-11 13:43:30 +0000

    \

--- File Week 3/normalise.py
+++ File Week 3/normalise.py
@@ -4,8 +4,13 @@
 # Author: MAry Metcalfe
 
 raw_string = input("please enter a string: ")
-normalised_string = raw_string.strip ().lower()
+normalised_string = raw_string.strip().lower()
 
-print ("{} is normalised to : {}".format( raw_string, normalised_string))
+#print ("{} is normalised to : {}".format( raw_string, normalised_string))
+
+length_of_raw_string = len(raw_string)
+length_of_normalised_string = len(normalised_string)
 
 print ("the sting normalised is: {}".format(normalised_string))
+
+print ("We changed the length of the string from {} to {} charcters ".format(length_of_raw_string, length_of_normalised_string))
