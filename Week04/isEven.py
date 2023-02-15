96a9d2fdbab596ff9362b899eb78107ede83161e
Mary Metcalfe <G00425666@atu.ie>
2023-02-15 16:54:16 +0000

    \

---
+++ Week04/isEven.py
@@ -0,0 +1,11 @@
+# Flow
+# Even and odd numbers
+# ifs and elses
+# author: Mary Metcalfe
+
+number = int(input("enter and integer: "))
+
+if (number %  2)  == 0:
+    print (f"{number} is an even number")
+else:
+    print (f"{number} is an odd number")
