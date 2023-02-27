e7e4f74b41ded221c6531d11b5754a3dc5273d70
Mary Metcalfe <G00425666@atu.ie>
2023-02-27 12:51:58 +0000

    \

---
+++ Labs/topic 05 - datastructures/Month.py
@@ -0,0 +1,21 @@
+# messing with tuples
+# week 05
+# months in the year - sub section summer months
+# author : Mary Metcalfe
+
+months = ("January",
+            "Febuary",
+            "March",
+            "April",
+            "May",
+            "June",
+            "July",
+            "September",
+            "October",
+            "November",
+            "December"
+)
+
+summer = months [4:7]
+for months in summer: 
+    print (months)
