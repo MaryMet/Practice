ddf83c227a67b59d51dee0c58d5571fe567ed099
Mary Metcalfe <G00425666@atu.ie>
2023-03-16 16:44:53 +0000

    \

--- File Week 08/salaries.py
+++ File Week 08/salaries.py
@@ -8,6 +8,15 @@ min_salary = 20000
 max_salary = 80000
 no_of_entries = 10
 
-saleries = np.random.randint(min_salary,max_salary,no_of_entries)
+salaries = np.random.randint(min_salary,max_salary,no_of_entries)
 
-print (saleries)
+salaries_plus = salaries + 5000
+
+salaries_mult = salaries * 1.05
+
+new_salaries = salaries_mult.astype(int)
+
+print (salaries)
+print (salaries_plus)
+print (salaries_mult)
+print (new_salaries)
