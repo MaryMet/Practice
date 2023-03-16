84345c22db370f8dc4fc9eead99b3101e590f103
Mary Metcalfe <G00425666@atu.ie>
2023-03-16 17:54:55 +0000

    \

---
+++ File Week 08/age.py
@@ -0,0 +1,28 @@
+# salary and ages
+# plotting
+# Author: Mary Metcalfe
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+min_salary = 20000
+max_salary = 80000
+no_of_entries = 100
+
+np.random.seed(1)
+
+salaries = np.random.randint(min_salary,max_salary,no_of_entries)
+
+ages = np.random.randint(low =21, high = 65, size = no_of_entries)
+
+plt.scatter (ages, salaries, label = "salaries")
+
+xpoints = np.array(range(1,101))
+ypoints = xpoints * xpoints
+
+plt.plot(xpoints, ypoints, color = "purple", label = "x squared")
+plt.title("random plot")
+plt.xlabel("Salaries")
+plt.ylabel("Age")
+plt.legend()
+plt.savefig('prettier-plot.png')
