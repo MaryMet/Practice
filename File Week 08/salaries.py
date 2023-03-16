28daf35ab223cc72a52a47c489a66df3ef86d3a0
Mary Metcalfe <G00425666@atu.ie>
2023-03-16 16:38:19 +0000

    \

---
+++ File Week 08/salaries.py
@@ -0,0 +1,13 @@
+# messing with numpy
+# salaries
+# Author: Mary Metcalfe
+
+import numpy as np
+
+min_salary = 20000
+max_salary = 80000
+no_of_entries = 10
+
+saleries = np.random.randint(min_salary,max_salary,no_of_entries)
+
+print (saleries)
