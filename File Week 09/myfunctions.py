1f6b19193c876230c83cc83ee891ddab7e2c91bf
Mary Metcalfe <G00425666@atu.ie>
2023-03-24 18:51:11 +0000

    \

--- File Week 09/myfunctions.py
+++ File Week 09/myfunctions.py
@@ -1,9 +1,22 @@
 # Week 09 - Errors, testing and logging
 # fibonacci sequence
 # Author : Mary Metcalfe        
+import logging
+logging.basicConfig (level = logging.DEBUG)
 
 def fibonacci (number):
-    return[]
+    a = 0
+    b = 1
+
+    fibonacci_sequence = [0]
+    for i in range (1, number):
+        fibonacci_sequence.append(b)
+        a, b=b, a+b
+        logging.debug("%d:% 5s", number, fibonacci_sequence )
+    if number == 0:
+        return []
+    if number <0:
+        raise value_error ('Number must be > 0')  
 
 return7 = [0,1,1,2,3,5,8]
 return11 = [0,1,1,1,2,3,5,8,13,21,34,55]
@@ -12,6 +25,15 @@ assert fibonacci (7) == return7
 assert fibonacci (11) == return11
 assert fibonacci (0) == []
 assert fibonacci (0) ==[0]
+
+#try:
+  #  fibonacci (-1)
+#except: value_error
+#pass
+    #else:
+     #assert false , "fibonacci missing value_error"
+
+
     
 
 
