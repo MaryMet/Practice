f624f1b5ecae7e2dc4322d109a750069d50919f1
Mary Metcalfe <G00425666@atu.ie>
2023-03-24 18:29:46 +0000

    \

---
+++ File Week 09/myfunctions.py
@@ -0,0 +1,20 @@
+# Week 09 - Errors, testing and logging
+# fibonacci sequence
+# Author : Mary Metcalfe        
+
+def fibonacci (number):
+    return[]
+
+return7 = [0,1,1,2,3,5,8]
+return11 = [0,1,1,1,2,3,5,8,13,21,34,55]
+
+assert fibonacci (7) == return7
+assert fibonacci (11) == return11
+assert fibonacci (0) == []
+assert fibonacci (0) ==[0]
+    
+
+
+
+#if _name_=='_main'
+#print ("All Good")
